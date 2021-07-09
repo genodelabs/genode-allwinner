6fa75fa8b754ad91ff89745b959b6f94fd236f8d
Norman Feske <norman.feske@genode-labs.com>
2021-07-09 13:14:07 +0200

    Recipes for api/a64_linux and src/a64_emac_nic_drv

---
+++ recipes/api/a64_linux/content.mk
@@ -0,0 +1,143 @@
+#
+# Content hosted in the dde_linux repository
+#
+
+MIRRORED_FROM_DDE_LINUX := src/lib/lx_emul \
+                           src/lib/lx_kit \
+                           src/include/lx_emul \
+                           src/include/lx_user \
+                           src/include/spec/arm_64/lx_kit \
+                           src/include/lx_kit
+
+content: $(MIRRORED_FROM_DDE_LINUX)
+$(MIRRORED_FROM_DDE_LINUX):
+	mkdir -p $(dir $@); cp -r $(GENODE_DIR)/repos/dde_linux/$@ $(dir $@)
+
+
+#
+# Content hosted in the allwinner repository
+#
+
+MIRRORED_FROM_REP_DIR := lib/mk/spec/arm_v8/a64_linux_generated.mk \
+                         src/a64_linux/target.inc \
+                         lib/import/import-a64_lx_emul.mk \
+                         lib/mk/a64_lx_emul.mk \
+                         src/include/lx_emul/initcall_order.h
+
+content: $(MIRRORED_FROM_REP_DIR)
+$(MIRRORED_FROM_REP_DIR):
+	$(mirror_from_rep_dir)
+
+
+#
+# Content from the Linux source tree
+#
+
+PORT_DIR := $(call port_dir,$(REP_DIR)/ports/a64_linux)
+LX_REL_DIR := src/linux
+LX_ABS_DIR := $(addsuffix /$(LX_REL_DIR),$(PORT_DIR))
+
+# ingredients needed for creating a Linux build directory / generated headers
+LX_FILES += Makefile \
+            Kbuild \
+            scripts/Makefile \
+            scripts/Kbuild.include \
+            scripts/subarch.include \
+            scripts/mkmakefile \
+            scripts/Makefile.build \
+            scripts/basic/Makefile \
+            scripts/Makefile.extrawarn \
+            scripts/Makefile.lib \
+            scripts/Makefile.host \
+            scripts/Makefile.asm-generic \
+            scripts/config \
+            scripts/basic/fixdep.c \
+            scripts/cc-version.sh \
+            scripts/ld-version.sh \
+            scripts/dtc \
+            scripts/kconfig/merge_config.sh \
+            scripts/sorttable.h \
+            scripts/sorttable.c \
+            scripts/setlocalversion \
+            scripts/mod \
+            scripts/checksyscalls.sh \
+            scripts/atomic/check-atomics.sh \
+            tools/include/tools \
+            arch/arm64/Makefile \
+            arch/arm64/configs \
+            kernel/configs/tiny.config \
+            arch/arm64/boot/dts \
+            arch/arm64/include/asm/Kbuild \
+            arch/x86/entry/syscalls/syscall_32.tbl \
+            include/asm-generic/Kbuild \
+            include/linux/compiler-version.h \
+            include/linux/license.h \
+            include/linux/kbuild.h \
+            include/linux/atomic-fallback.h \
+            include/uapi/asm-generic/Kbuild
+
+LX_SCRIPTS_KCONFIG_FILES := $(notdir $(wildcard $(LX_ABS_DIR)/scripts/kconfig/*.c)) \
+                            $(notdir $(wildcard $(LX_ABS_DIR)/scripts/kconfig/*.h)) \
+                            Makefile lexer.l parser.y
+LX_FILES += $(addprefix scripts/kconfig/,$(LX_SCRIPTS_KCONFIG_FILES)) \
+
+LX_FILES += $(shell cd $(LX_ABS_DIR); find -name "Kconfig*" -printf "%P\n")
+
+# needed for generated/asm-offsets.h
+LX_FILES += kernel/bounds.c \
+            kernel/time/timeconst.bc \
+            arch/arm64/kernel/asm-offsets.c \
+            arch/arm64/include/asm/current.h \
+            arch/arm64/include/asm/memory.h \
+            arch/arm64/include/asm/irqflags.h \
+            arch/arm64/include/asm/page.h \
+            arch/arm64/include/asm/pgtable.h \
+            arch/arm64/include/asm/mte.h \
+            arch/arm64/include/asm/fixmap.h \
+            arch/arm64/include/asm/boot.h \
+            arch/arm64/include/asm/signal32.h \
+            arch/arm64/include/asm/smp_plat.h \
+            arch/arm64/include/asm/suspend.h \
+            include/uapi/linux/arm_sdei.h \
+            include/acpi \
+            include/asm-generic/current.h \
+            include/asm-generic/memory_model.h \
+            include/asm-generic/qspinlock.h \
+            include/asm-generic/fixmap.h \
+            include/asm-generic/pgtable_uffd.h \
+            include/linux/arm_sdei.h \
+            include/linux/arm-smccc.h \
+            include/linux/cper.h \
+            include/linux/pgtable.h
+
+# needed for gen_crc32table
+LX_FILES += lib/gen_crc32table.c \
+            lib/crc32.c
+
+content: src/linux/include/linux/kvm_host.h
+src/linux/include/linux/kvm_host.h: # cut dependencies from kvm via dummy header
+	mkdir -p $(dir $@)
+	touch $@
+
+# prevent build of vdso as side effect of the linux-build directory preparation
+content: src/linux/arch/arm64/kernel/vdso/Makefile
+src/linux/arch/arm64/kernel/vdso/Makefile:
+	mkdir -p $(dir $@)
+	echo "default:" > $@
+	echo "include/generated/vdso-offsets.h:" >> $@
+	echo "arch/arm64/kernel/vdso/vdso.so:" >> $@
+
+# add content listed in the repository's source.list or dep.list files
+LX_FILE_LISTS := $(shell find $(REP_DIR) -name dep.list -or -name source.list)
+LX_FILES += $(shell cat $(LX_FILE_LISTS))
+LX_FILES := $(sort $(LX_FILES))
+MIRRORED_FROM_PORT_DIR += $(addprefix $(LX_REL_DIR)/,$(LX_FILES))
+
+content: $(MIRRORED_FROM_PORT_DIR)
+$(MIRRORED_FROM_PORT_DIR):
+	mkdir -p $(dir $@)
+	cp -r $(addprefix $(PORT_DIR)/,$@) $@
+
+content: LICENSE
+LICENSE:
+	cp $(PORT_DIR)/src/linux/COPYING $@
