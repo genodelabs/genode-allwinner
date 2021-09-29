694a0110f6c5e9933ce5a31e516dcbada57bd599
Norman Feske <norman.feske@genode-labs.com>
2021-09-29 13:40:30 +0200

    core: adapt to base-hw-internal changes
    Issue genodelabs/genode#4217

--- src/core/board/pine_a64lts/board.h
+++ src/core/board/pine_a64lts/board.h
@@ -17,6 +17,7 @@
 #include <hw/spec/arm_64/pine_a64lts_board.h>
 #include <spec/arm/generic_timer.h>
 #include <spec/arm/virtualization/gicv2.h>
+#include <spec/arm_v8/cpu.h>
 #include <spec/arm_64/cpu/vm_state_virtualization.h>
 #include <spec/arm/virtualization/board.h>
 
