50eced1c129120f60f26c50d81df7a1d0061b7f1
Norman Feske <norman.feske@genode-labs.com>
2021-11-20 22:53:56 +0100

    framebuffer/de: revoke PIO and R_PIO device access
    With this patch, the framebuffer driver's access to GPIO pins is
    mediated by the a64_pin_driver component.

--- src/drivers/framebuffer/de/target.mk
+++ src/drivers/framebuffer/de/target.mk
@@ -4,6 +4,10 @@ REQUIRES = arm_v8a
 LIBS     = base blit a64_lx_emul
 SRC_CC  += main.cc
 SRC_C   += fb.c lx_user.c
+SRC_C   += lx_emul/pin.c
+SRC_C   += lx_emul/a64/pio.c
+
+vpath lx_emul/a64/pio.c $(REP_DIR)/src/lib
 
 BOARDS := pinephone
 
