ac4c6e44307afae5c335fd3cbb06f9e549d48e1b
Norman Feske <norman.feske@genode-labs.com>
2022-01-11 12:02:29 +0100

    Adjustment to upstream platform-driver changes
    Issue genodelabs/genode#4359

--- src/drivers/platform/a64/target.mk
+++ src/drivers/platform/a64/target.mk
@@ -8,8 +8,13 @@ SRC_CC  += session_component.cc
 SRC_CC  += root.cc
 LIBS     = base
 
-PLATFORM_ARM_DIR := $(call select_from_repositories,src/drivers/platform/spec/arm)
-INC_DIR           = $(PRG_DIR) $(PLATFORM_ARM_DIR)
+#
+# Determine location of generic platform-driver code.
+# (either in os repository or in the a64_platform_drv src archive)
+#
+GENERIC_DIR := $(dir $(call select_from_repositories,src/drivers/platform/clock.h))
+
+INC_DIR = $(PRG_DIR) $(GENERIC_DIR)
 
 vpath main.cc $(PRG_DIR)
-vpath %.cc $(PLATFORM_ARM_DIR)
+vpath %.cc $(GENERIC_DIR)
