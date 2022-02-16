41b0ae8e1099a1fa555276912e9c1e31e0cabf61
Norman Feske <norman.feske@genode-labs.com>
2022-02-16 14:49:52 +0100

    Enable SMP

--- lib/mk/spec/arm_v8/bootstrap-hw-pine_a64lts.mk
+++ lib/mk/spec/arm_v8/bootstrap-hw-pine_a64lts.mk
@@ -9,7 +9,7 @@ SRC_CC  += lib/base/arm_64/kernel/interface.cc
 SRC_CC  += spec/64bit/memory_map.cc
 SRC_S   += bootstrap/spec/arm_64/crt0.s
 
-NR_OF_CPUS = 1
+NR_OF_CPUS = 4
 
 vpath spec/64bit/memory_map.cc $(call select_from_repositories,src/lib/hw)
 
