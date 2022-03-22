b30e377dff80de4d8c17913e6cb7034fde7d2d82
Norman Feske <norman.feske@genode-labs.com>
2022-03-22 14:15:15 +0100

    uart/ns16550: rename Terminal::Uart to Uart::Main

--- src/drivers/uart/ns16550/main.cc
+++ src/drivers/uart/ns16550/main.cc
@@ -1,5 +1,5 @@
-/**
- * \brief  Driver that connects the Uart to a terminal session
+/*
+ * \brief  Driver that connects a UART to a terminal session
  * \author Sebastian Sumpf
  * \date   2022-02-17
  */
@@ -19,15 +19,15 @@
 
 #include "ns16550.h"
 
-namespace Terminal {
+namespace Uart {
 
 	using namespace Genode;
-	struct Uart;
+	struct Main;
 
 };
 
 
-struct Terminal::Uart
+struct Uart::Main
 {
 	Env &_env;
 
@@ -41,12 +41,12 @@ struct Terminal::Uart
 
 	/* serial clock is 25MHz and 115200 baud  */
 	Ns16550_uart         _uart { _device, 25000000, 115200 };
-	Signal_handler<Uart> _irq_handler { _env.ep(), *this, &Uart::_handle_irq };
+	Signal_handler<Main> _irq_handler { _env.ep(), *this, &Main::_handle_irq };
 
 	Terminal::Connection _terminal { _env };
-	Signal_handler<Uart> _terminal_read { _env.ep(), *this, &Uart::_handle_read };
+	Signal_handler<Main> _terminal_read { _env.ep(), *this, &Main::_handle_read };
 
-	Uart(Genode::Env &env) : _env(env)
+	Main(Genode::Env &env) : _env(env)
 	{
 		_irq.sigh(_irq_handler);
 		_terminal.read_avail_sigh(_terminal_read);
@@ -82,6 +82,6 @@ struct Terminal::Uart
 
 void Component::construct(Genode::Env &env)
 {
-	static Terminal::Uart uart(env);
+	static Uart::Main uart(env);
 
 }
