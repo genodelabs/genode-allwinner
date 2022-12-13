6a1eaf67402f5af60185d3065d12d9c8b54ce5fd
Norman Feske <norman.feske@genode-labs.com>
2022-12-13 15:42:33 +0100

    Load SCP firmware for PMIC by boot loader
    The patch moves the firmware commands needed for accessing the
    power-management IC (PMIC) from the platform driver to the SCP
    image. This speeds up the platform-driver initialization by half
    a second.
    
    Note that u-boot must be rebuilt after applying this patch:
    
      make u-boot/pine BOARD=pinephone
    
    Issue #14

--- src/drivers/platform/a64/main.cc
+++ src/drivers/platform/a64/main.cc
@@ -66,62 +66,7 @@ void Driver::Main::_load_scp_firmware()
 {
 	char const *firmware[] = {
 
-		/* utilities */
-		": /        0 swap um/mod swap drop ; "
-		": invert   not ; "
-		": lshift   FOR AFT dup + THEN NEXT ; "
-		": rshift   FOR AFT 2 / THEN NEXT ; "
-		": bitmask  1 swap lshift 1 - ; "
-		": mdelay   1ab * FOR NEXT ; "
-		": udelay   1 rshift FOR NEXT ;",
-
-		/* bitfield access */
-		": bf!  bitmask rot >r over lshift >r lshift r@ and r> invert r@ @ and or r> ! ;",
-		": bf@  bitmask over lshift rot @ and swap rshift ;",
-		": b!   1 bf! ;",
-		": b@   1 bf@ ;",
-
-		/* reduced serial bus (RSB) register addresses */
-		": rsb 1f03400    ; "
-		": ctrl  rsb      ; "
-		": ccr   rsb 4  + ; "
-		": stat  rsb c  + ; "
-		": daddr rsb 10 + ; "
-		": data  rsb 1c + ; "
-		": pmcr  rsb 28 + ; "
-		": cmd   rsb 2c + ; "
-		": saddr rsb 30 + ; ",
-
-		": rtsaddr.bf  saddr 10 8 ;",
-		": sladdr.bf   saddr 0 10 ;",
-
-		": wait_stat    BEGIN stat @ 1 = UNTIL 1 stat ! ;",
-		": start_trans  1 ctrl 7 b!  wait_stat ;",
-
-		/* enable prcm_r two-wire interface (TWI) */
-		": prcm_r_twi 1f01400 28 + ;",
-		"prcm_r_twi @ 40 or prcm_r_twi !",
-
-		/* RSB reset (must be a complied word because of BEGIN/UNTIL) */
-		": reset 1 ctrl 0 b!  BEGIN ctrl @ 0 = UNTIL ; reset",
-
-		/* config CCR */
-		"1 ccr 0 b!  1 ccr 1 b!  2 ccr 8 2 bf!",
-
-		/* trigger PMU init sequence */
-		"7c pmcr 10 8 bf!  1 pmcr 1f b!  wait_stat",
-
-		/* set RSB slave */
-		"e8 cmd !  2d rtsaddr.bf bf!  3a3 sladdr.bf bf!  start_trans",
-
-		/* ( reg -- ) prepare PMIC read/write operation */
-		": setup_rw  daddr !  2d rtsaddr.bf bf!  0 sladdr.bf bf! ;",
-
-		/* ( reg -- value ) read PMIC register */
-		": pmic@  setup_rw  8b cmd !  0 data !  start_trans data @ ;",
-
-		/* ( value reg -- ) write PMIC register */
-		": pmic!  setup_rw  4e cmd !  data !  start_trans ;",
+		/* testing ground for SCP firmware customizations */
 	};
 
 	for (char const *command : firmware) {
