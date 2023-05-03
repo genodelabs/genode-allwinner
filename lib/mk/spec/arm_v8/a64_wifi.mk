1854a9685bcaefa73a0d203ca144ca6bf84e2c1d
Josef Söntgen <josef.soentgen@genode-labs.com>
2023-05-03 14:27:57 +0200

    a64_wifi_drv: adapt to wifi move
    Issue genodelabs/genode#4861.

--- lib/mk/spec/arm_v8/a64_wifi.mk
+++ lib/mk/spec/arm_v8/a64_wifi.mk
@@ -1,71 +1,34 @@
 REQUIRES := arm_v8a
 
-ifeq ($(CONTRIB_DIR),)
-PC_REPO_LIB_DIR  := $(REP_DIR)/src/lib/pc
-ORIGINAL_LIB_DIR := $(REP_DIR)/src/lib/wifi
-else
-# For now use the 'base' repository to look up the 'pc' repository
-# so we do not need to add that in the build.conf. This should be
-# changed back to 'select_from_repositories' when the common parts
-# of the wifi library are moved to 'dde_linux'.
-PC_REPO_DIR      := $(BASE_DIR)/../pc
-PC_REPO_LIB_DIR  := $(PC_REPO_DIR)/src/lib/pc
-ORIGINAL_LIB_DIR := $(PC_REPO_DIR)/src/lib/wifi
-endif
+include $(call select_from_repositories,lib/mk/wifi.inc)
 
+TARGET_LIB_DIR := $(REP_DIR)/src/lib/a64_wifi
 
-TARGET_LIB_DIR   := $(REP_DIR)/src/lib/a64_wifi
+LIBS    += a64_linux_generated a64_lx_emul
 
-DRIVER := wifi
+DRIVER                 := wifi
 BOARDS                 := pinephone
 DTS_EXTRACT(pinephone) := --select mmc1
 
-
-SHARED_LIB := yes
-
-LD_OPT  += --version-script=$(ORIGINAL_LIB_DIR)/symbol.map
-
-LIBS    += base jitterentropy a64_linux_generated a64_lx_emul
 INC_DIR += $(TARGET_LIB_DIR)
-INC_DIR += $(ORIGINAL_LIB_DIR)
-SRC_CC  += wlan.cc
-SRC_CC  += firmware.cc
-SRC_CC  += socket_call.cc
+
 SRC_CC  += lx_emul/random.cc
 SRC_CC  += dtb_helper.cc
 
 SRC_C   += clock.c
-SRC_C   += dummies.c
-SRC_C   += lx_emul.c
-SRC_C   += lx_user.c
-SRC_C   += uplink.c
-SRC_C   += lx_socket_call.c
-SRC_C   += $(notdir $(wildcard $(REP_DIR)/src/lib/a64_wifi/generated_dummies.c))
+SRC_C   += $(notdir $(wildcard $(TARGET_LIB_DIR)/generated_dummies.c))
 
 
+SRC_C += lx_emul/shadow/drivers/char/random.c
+SRC_C += lx_emul/shadow/fs/libfs.c
 SRC_C += lx_emul/shadow/mm/page_alloc.c
+SRC_C += lx_emul/shadow/mm/vmalloc.c
+
 SRC_C += lx_emul/a64/common_dummies.c
 SRC_C += lx_emul/a64/pmic.c
 
 vpath lx_emul/a64/common_dummies.c $(REP_DIR)/src/lib
 vpath lx_emul/a64/pmic.c           $(REP_DIR)/src/lib
-vpath lx_emul/a64/sched.c          $(REP_DIR)/src/lib
-
-SRC_C   += lx_emul/shadow/mm/page_alloc.c
-
-#
-# The following block of compilation units is so far part of the
-# '*_lx_emul' library. Some parts could be merged into the generic
-# DDE Linux part.
-#
-
-SRC_C   += lx_emul/mapping.c
-SRC_C   += lx_emul/page_alloc.c
-SRC_C   += lx_emul/sched_core.c
-SRC_C   += lx_emul/vmalloc.c
-SRC_C   += lx_emul/shadow/fs/libfs.c
-SRC_C   += lx_emul/shadow/drivers/char/random.c
-SRC_C   += lx_emul/shadow/mm/dmapool.c
 
 #
 # RTL8723cs extra flags obtained by instrumenting 'rtl8723cs/Makefile'
@@ -118,27 +81,8 @@ CC_C_OPT += \
 CC_OPT_drivers/staging/rtl8723cs/core/rtw_debug += -Wno-builtin-macro-redefined \
                                                    -D__DATE__=\"0\" -D__TIME__=\"0\"
 
-CC_C_OPT += -DCONFIG_RFKILL_INPUT
-
-CC_OPT_lx_socket_call     += -DKBUILD_MODNAME='"lx_socket_call"'
-CC_OPT_dummies            += -DKBUILD_MODNAME='"dummies"'
-CC_OPT_generated_dummies  += -DKBUILD_MODNAME='"generated_dummies"'
 CC_OPT_net/mac80211/trace += -I$(LX_SRC_DIR)/net/mac80211
 CC_OPT_net/wireless/trace += -I$(LX_SRC_DIR)/net/wireless
 
 vpath %.c  $(TARGET_LIB_DIR)
 vpath %.cc $(TARGET_LIB_DIR)
-vpath %.c  $(ORIGINAL_LIB_DIR)
-vpath %.cc $(ORIGINAL_LIB_DIR)
-
-vpath %.c $(PC_REPO_LIB_DIR)
-
-$(LIB).lib.so: $(ORIGINAL_LIB_DIR)/symbol.map
-
-#
-# Genode C-API backends
-#
-
-SRC_CC  += genode_c_api/uplink.cc
-
-vpath genode_c_api/uplink.cc $(subst /genode_c_api,,$(call select_from_repositories,src/lib/genode_c_api))
--- recipes/src/a64_wifi_drv/content.mk
+++ recipes/src/a64_wifi_drv/content.mk
@@ -1,33 +1,19 @@
 #
-# Driver portions
+# Specific driver portions
 #
 
 LIB_MK := $(foreach SPEC,arm_v8,lib/mk/spec/$(SPEC)/a64_wifi.mk)
 
-MIRROR_FROM_REP_DIR := src/drivers/wifi/a64 \
-                       src/lib/lx_emul/a64 \
+MIRROR_FROM_REP_DIR := src/lib/lx_emul/a64 \
                        src/include \
                        src/lib/a64_wifi \
                        $(LIB_MK)
 
+#
+# Generic driver poritions from dde_linux and os
+#
+
 MIRROR_FROM_OS_DIR  := src/lib/genode_c_api/uplink.cc
-
-#
-# PC portions
-#
-
-PC_REP_DIR := $(GENODE_DIR)/repos/pc
-
-MIRROR_FROM_PC_DIR := \
-                      lib/mk/vfs_wifi.mk \
-                      $(shell cd $(PC_REP_DIR); find src/lib/pc/lx_emul -type f) \
-                      $(shell cd $(PC_REP_DIR); find src/drivers/wifi -type f) \
-                      $(shell cd $(PC_REP_DIR); find src/lib/wifi -maxdepth 1 -type f) \
-                      $(shell cd $(PC_REP_DIR); find src/lib/vfs/wifi -type f)
-
-#
-# DDE Linux portions (wpa_supplicant, libnl)
-#
 
 DDE_LINUX_REP_DIR  := $(GENODE_DIR)/repos/dde_linux
 
@@ -35,15 +21,19 @@ WS_PORT_DIR    := $(call port_dir,$(DDE_LINUX_REP_DIR)/ports/wpa_supplicant)
 LIBNL_PORT_DIR := $(call port_dir,$(DDE_LINUX_REP_DIR)/ports/libnl)
 
 DDE_LINUX_LIB_MK := \
-          $(addprefix lib/mk/,libnl.inc libnl_include.mk) \
+          $(addprefix lib/mk/,libnl.inc libnl_include.mk wifi.inc vfs_wifi.mk) \
           $(foreach SPEC,arm_64,lib/mk/spec/$(SPEC)/libnl.mk) \
           $(addprefix lib/mk/,wpa_driver_nl80211.mk wpa_supplicant.mk)
 
 MIRROR_FROM_DDE_LINUX_DIR := $(DDE_LINUX_LIB_MK) \
+                             src/drivers/wifi \
                              lib/import/import-libnl_include.mk \
                              lib/import/import-libnl.mk \
+                             lib/symbols/wifi \
                              include/wifi \
                              $(shell cd $(DDE_LINUX_REP_DIR); find src/lib/libnl -type f) \
+                             $(shell cd $(DDE_LINUX_REP_DIR); find src/lib/vfs/wifi -type f) \
+                             $(shell cd $(DDE_LINUX_REP_DIR); find src/lib/wifi -type f) \
                              $(shell cd $(DDE_LINUX_REP_DIR); find src/lib/wpa_driver_nl80211 -type f) \
                              $(shell cd $(DDE_LINUX_REP_DIR); find src/lib/wpa_supplicant -type f)
 
@@ -51,15 +41,10 @@ MIRROR_FROM_WS_PORT_DIR    := $(shell cd $(WS_PORT_DIR); find src/app/wpa_suppli
 MIRROR_FROM_LIBNL_PORT_DIR := $(shell cd $(LIBNL_PORT_DIR); find src/lib/libnl -type f)
 
 content: $(MIRROR_FROM_REP_DIR) $(MIRROR_FROM_OS_DIR) $(MIRROR_FROM_DDE_LINUX_DIR) \
-         $(MIRROR_FROM_WS_PORT_DIR) $(MIRROR_FROM_LIBNL_PORT_DIR) \
-         $(MIRROR_FROM_PC_DIR) cleanup-wpa
+         $(MIRROR_FROM_WS_PORT_DIR) $(MIRROR_FROM_LIBNL_PORT_DIR) cleanup-wpa
 
 $(MIRROR_FROM_REP_DIR):
 	$(mirror_from_rep_dir)
-
-$(MIRROR_FROM_PC_DIR):
-	mkdir -p $(dir $@)
-	cp -r $(GENODE_DIR)/repos/pc/$@ $@
 
 $(MIRROR_FROM_OS_DIR):
 	mkdir -p $(dir $@)
