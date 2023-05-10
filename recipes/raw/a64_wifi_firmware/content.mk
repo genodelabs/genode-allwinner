fdedce266a5b2c006b1b0c8c8a5f1908f7808572
Josef Söntgen <josef.soentgen@genode-labs.com>
2023-05-10 17:28:57 +0200

    Adapt to firmware from tar archive
    Issue genodelabs/genode#4861.

--- recipes/raw/a64_wifi_firmware/content.mk
+++ recipes/raw/a64_wifi_firmware/content.mk
@@ -1,6 +1,6 @@
 PORT_DIR := $(call port_dir,$(GENODE_DIR)/repos/dde_linux/ports/linux-firmware)
 
-content: ucode_files LICENSE.wifi_drv
+content: ucode_files LICENSE.wifi_drv a64_wifi_firmware.tar
 
 
 .PHONY: ucode_files
@@ -10,3 +10,7 @@ ucode_files:
 
 LICENSE.wifi_drv:
 	cp $(PORT_DIR)/firmware/LICENSE.regulatory_db $@
+
+a64_wifi_firmware.tar: ucode_files LICENSE.wifi_drv
+	tar --mtime='2023-05-10 00:00Z' --remove-files \
+	    -cf $@ -C . *.*
