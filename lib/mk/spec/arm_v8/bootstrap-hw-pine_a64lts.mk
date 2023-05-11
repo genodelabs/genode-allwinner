3b39abdaa53faa9fdfb3552d050c371e41de1dd9
Stefan Kalkowski <stefan.kalkowski@genode-labs.com>
2023-05-11 12:07:09 +0200

    hw: prevent alignment faults in bootstrap
    Enable strict-alignment to compile bootstrap code, which cannot handle
    alignment faults during initialization properly.
    
    Ref genodelabs/genode#4827

--- lib/mk/spec/arm_v8/bootstrap-hw-pine_a64lts.mk
+++ lib/mk/spec/arm_v8/bootstrap-hw-pine_a64lts.mk
@@ -5,12 +5,7 @@ REP_INC_DIR += src/bootstrap/board/pine_a64lts
 SRC_CC  += bootstrap/board/pine_a64lts/platform.cc
 SRC_CC  += bootstrap/spec/arm/gicv2.cc
 SRC_CC  += bootstrap/spec/arm_64/cortex_a53_mmu.cc
-SRC_CC  += lib/base/arm_64/kernel/interface.cc
-SRC_CC  += spec/64bit/memory_map.cc
-SRC_S   += bootstrap/spec/arm_64/crt0.s
-
-vpath spec/64bit/memory_map.cc $(call select_from_repositories,src/lib/hw)
 
 vpath bootstrap/% $(REP_DIR)/src
 
-include $(call select_from_repositories,lib/mk/bootstrap-hw.inc)
+include $(call select_from_repositories,lib/mk/spec/arm_v8/bootstrap-hw.inc)
