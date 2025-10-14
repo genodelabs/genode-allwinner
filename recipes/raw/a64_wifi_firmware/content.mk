b3263d7e2b836e8c5f2bd8dca1b1e9d301cd865f
Christian Helmuth <christian.helmuth@genode-labs.com>
2025-10-14 16:09:03 +0200

    Generate reproducible tar archives

--- recipes/raw/a64_wifi_firmware/content.mk
+++ recipes/raw/a64_wifi_firmware/content.mk
@@ -11,7 +11,7 @@ ucode_files:
 LICENSE.wifi:
 	cp $(PORT_DIR)/firmware/LICENSE.regulatory_db $@
 
+include $(GENODE_DIR)/repos/base/recipes/content.inc
+
 a64_wifi_firmware.tar: ucode_files LICENSE.wifi
-	tar --mtime='2023-05-10 00:00Z' --remove-files \
-	    --owner=0 --group=0 --numeric-owner --mode='go=' \
-	    -cf $@ -C . *.*
+	$(TAR) --remove-files -cf $@ -C . *.*
